103f49121d8895e24d7dc5b09d9d82e7ac5083a2
Mark Kershaw <kershaw.mark@gmail.com>
2016-11-17 16:20:04 +0000

    update

--- types.hs
+++ types.hs
@@ -16,6 +16,8 @@ There are a number of concepts associated with the type system in haskell that w
 * newtype
 
 -}
+-- Defaults on Record fields
+import Data.Default
 
 -- We allow Haskell to infer the types of all the following
 -- To view the actual type chosen in ghci use :t (e.g :t label1)
@@ -85,3 +87,77 @@ func6::MyType->MyType
 func6 x = if x == RED then RED else x
 
 
+-- ***********************************************************
+-- Let's start to look at more interesting data types
+
+-- This is a Tree DS. See tree.hs for a set of API and tests on this
+-- NOTE: This is an example of a parameterised Type, or a Type constructor
+-- You cannot use Tree as a type anywhere, you have to make a concrete type out of it
+-- by specifying what the type of the parameter is e.g (Tree String) or (Tree Int)
+data Tree a = NIL | Tree a (Tree a) (Tree a) deriving (Show)
+
+
+-- MyRec1 defines a record structure type
+-- NOTE: The MyRec1 to the left of the = is the Type. MyRec1 to the right is the Value constructor.
+-- In this case we are using the same name but they do not have to be. See MyType above.
+--
+-- Types created using the haskell data mechanism are referred to as algebraic data types.
+-- The algebraic refers to the fact that you can specify the types using AND and OR algebra
+-- data MyType = Val1 INT INT | Val2 String OtherType
+--
+data MyRec1 = MyRec1 String Int Int deriving (Show)
+
+data1 = MyRec1 "Hello World" 1 2
+
+-- The above MyRec1 works but it is no very readable. The fields of our Value MyRec1 are just referred to as String, Int and Int
+-- To make it more readable we can use the haskell type mechanism to create named aliased for types
+type CustomerName=String
+type ID=Int
+type Age=Int
+data MyRec2 = MyRec2 CustomerName ID Age deriving (Show)
+
+data2 = MyRec2 "Hello World" 1 2
+
+-- type aliases make MyRec2 slightly more readable but it is still not great. When we call the type we still need to pass parameters to the value constructor
+-- in the correct order. i.e. CustomerName first, ID second etc
+
+-- Another possibility is to use Record syntax
+
+data MyRec3 = MyRec3 {companyName :: CustomerName, custId :: ID, age :: Age} deriving (Show)
+
+data3 = MyRec3 {companyName="Oracle systems", custId=5678, age=50}
+
+-- You can then pattern match to pull things out of the structure
+-- Below gets the companyName field into theName
+MyRec3 {companyName=theName, custId=_, age=_} = data3
+
+-- You can just select the fields that you want as well (which is nice)
+MyRec3 {companyName=theName2} = data3
+
+-- Defaults on Record fields
+-- NOTE: The fields in the record need to be uniquely named. So for this artificial test I had to add 2 to the end of each else they clash with
+-- names on MyRec3
+data MyRec4 = MyRec4 {companyName2 :: CustomerName, custId2 :: ID, age2 :: Age} deriving (Show)
+
+-- You need import Data.Default
+-- NOTE: This is not installed by default on a standard haskell ghci environment.
+-- You need to install using the haskell package manager cabal
+-- cabal install data-default
+-- You also need to stop/start the ghci terminal
+instance Default MyRec4 where
+  def = MyRec4 "Default CustName" 9999 21
+          -- or whatever you want as the defaults
+
+data4 :: MyRec4
+data4 = def { companyName2 = "Acorn engineering" }
+
+
+-- Discussion on defaults http://stackoverflow.com/questions/31965666/how-do-i-set-default-values-in-a-record
+
+-- rather than use Data.Default you can just have your own default constructor
+-- I LIKE THIS. THIS would be my preference
+defRec = MyRec4 "Default CustName" 9999 21
+data5 = defRec { companyName2 = "Acorn engineering22" }
+
+-- The following would not work because you haven't given values for all the fields
+-- data6 = MyRec4 { companyName2 = "Acorn engineering33" } 
