dc8b46fc4fdb3adfa97c1bc3f7cdcaf33515a483
Mark Kershaw <kershaw.mark@gmail.com>
2016-11-19 16:29:06 +0000

    update

--- preMonad.hs
+++ preMonad.hs
@@ -9,9 +9,10 @@ NOTE: I am not expecting this to be easy though. The book has many comments from
 
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Lazy as L
-import Data.Char (isSpace)
+import Data.Char (isSpace,chr,isDigit)
 import Data.Int (Int64)
 import Data.Word (Word8)
+import Control.Applicative ((<$>)) -- When importing operators you need to enclose in () <$> is a opertor version of Functor::fmap
 
 data Greymap = Greymap {
       greyWidth :: Int
@@ -229,6 +230,9 @@ firstParser ==> secondParser  =  Parse chainedParser
   -- The thing that is confusing me is how the initState gets through into the chainedParser.
   -- I think I am over thinking this though.
   -- This ==> operator returns a higher order function (albeit wrapped in a type) and this takes a ParserState as a param
+  -- 
+  -- This way of chaining parsers together works left to right. i.e. firstParser works first and then passes results to secondParser
+  -- Compare this to function composition using (.) This works from right to left
   where chainedParser initState   =
           case runParse firstParser initState of
             Left errMessage ->
@@ -237,4 +241,149 @@ firstParser ==> secondParser  =  Parse chainedParser
                 runParse (secondParser firstResult) newState
 
 testparse3 = parse parseByte2 (L8.pack "foo")
+
+-- *************************************************************************************************************
+-- So let's start to look at replacing our PGM parser using the new abstractions and some more to be introsuced
+
+
+-- The next step on this journey is to take a look at Functors and how they will work with our parser.
+-- Quick reminder on Functors before we start. They are a generic for of the map function (map being specific to lists).
+-- They define a classtype that defines how to iteract through a data structure.
+
+data Tree a = Node (Tree a) (Tree a)
+            | Leaf a
+              deriving (Show)
+
+treeMap :: (a -> b) -> Tree a -> Tree b
+treeMap f (Leaf a)   = Leaf (f a)
+treeMap f (Node l r) = Node (treeMap f l) (treeMap f r)
+
+-- The following is the definition of Functor. 
+-- BUT this comes as part of Prelude Library so I will comment out here
+{- }
+class Functor f where
+    fmap :: (a -> b) -> f a -> f b
+-} 
+
+-- Now we define Functor for our Tree data type
+instance Functor Tree where
+    fmap = treeMap
+
+funcTest1 = fmap length ["foo","quux"]
+funcTest2 = fmap length (Node (Leaf "Livingstone") (Leaf "I presume"))
+
+-- We can think of fmap as a kind of lifting function. It takes a function over ordinary values a -> b and lifts it
+-- to become a function over containers f a -> f b, where f is the container type
+
+-- For here's our definition of fmap for Parse
+instance Functor Parse where
+    fmap f parser = parser ==> \result ->
+                    identity (f result)
+
+
+funcTest3 = parse (id <$> parseByte) input
+    where input = L8.pack "foo"
+
+funcTest4 = parse ((chr . fromIntegral) <$> parseByte) (L8.pack "foo")
+funcTest5 = parse (chr <$> fromIntegral <$> parseByte) (L8.pack "foo")
+
+w2c :: Word8 -> Char
+w2c = chr . fromIntegral
+
+parseChar :: Parse Char
+parseChar = w2c <$> parseByte
+
+funcTest6 = parse parseChar (L8.pack "foo")
+
+peekByte :: Parse (Maybe Word8)
+peekByte = (fmap fst . L.uncons . string) <$> getState
+
+funcTest7 = parse peekByte (L8.pack "foo")
+
+peekChar :: Parse (Maybe Char)
+peekChar = fmap w2c <$> peekByte
+
+funcTest8 = parse peekChar (L8.pack "foo")
+
+parseWhile :: (Word8 -> Bool) -> Parse [Word8]
+parseWhile p = (fmap p <$> peekByte) ==> \mp ->
+               if mp == Just True
+               then parseByte ==> \b ->
+                    (b:) <$> parseWhile p
+               else identity []
+
+-- Nearly there. This is the new version of the parser that we are aiming for
+-- It really does look quite alien when you come from an imperitive background.
+-- We have sort of created a DSL here for parsing binary files.
+-- The ==> ===>& and <$> all provide syntax sugar to allow you to express the chaining together of the Parse stages
+--
+-- What are my current thoughts? 
+-- * It is beginning to grow on me. I've had moments when I just wanted to give up on this because it looked such a mess
+-- but I'm glad that I haven't. This is the first time in a while with a programming language where I have really challenged myself 
+-- to understand the code.
+-- * It will be interesting when I revisit this code in the future whether it makes immediate sense next time.
+--
+-- I do feel however that understanding how code like this works is probably not for everyone.
+parseRawPGM =
+    parseWhileWith w2c notWhite ==> \header -> skipSpaces ==>&
+    assert (header == "P5") "invalid raw header" ==>&
+    parseNat ==> \width -> skipSpaces ==>&
+    parseNat ==> \height -> skipSpaces ==>&
+    parseNat ==> \maxGrey ->
+    parseByte ==>&
+    parseBytes (width * height) ==> \bitmap ->
+    identity (Greymap width height maxGrey bitmap)
+  where notWhite = (`notElem` " \r\n\t")
+
+-- This is a test for our parseRawPGM
+parseTest3 = 
+    do
+        byteStr <- L.readFile "test.pgm"
+        case parse parseRawPGM byteStr of
+            Left err -> putStrLn "Problem Parsing the PGM bitmap file"
+            Right bitmap -> putStrLn $ show bitmap
+
+-- Just need to define the final missing helper functions
+parseWhileWith :: (Word8 -> a) -> (a -> Bool) -> Parse [a]
+-- The fmap and <$> below can be confusing (we have this in a number of places below)
+-- Remember that <$> is just sugar for fmap
+-- best way of thinking about fmap is that it allows you to apply a function to items in a container and return a new container with results of function
+-- Our Parse container has Either types in it. To apply a function to the contents of the Either we need the second fmap
+-- Take a look at parseChar and peekChar above to see the difference
+-- 19th Nov 2016 - I am leaving this now still a little confused as to when we have to apply the double fmap
+parseWhileWith f p = fmap f <$> parseWhile (p . f)
+
+parseNat :: Parse Int
+parseNat = parseWhileWith w2c isDigit ==> \digits ->
+           if null digits
+           then bail "no more input"
+           else let n = read digits
+                in if n < 0
+                   then bail "integer overflow"
+                   else identity n
+
+(==>&) :: Parse a -> Parse b -> Parse b
+p ==>& f = p ==> \_ -> f
+
+skipSpaces :: Parse ()
+skipSpaces = parseWhileWith w2c isSpace ==>& identity ()
+
+assert :: Bool -> String -> Parse ()
+assert True  _   = identity ()
+assert False err = bail err
+
+parseBytes :: Int -> Parse L.ByteString
+parseBytes n =
+    getState ==> \st ->
+    let n' = fromIntegral n
+        (h, t) = L.splitAt n' (string st)
+        -- watch out for the record field accessor functions (string st) and offset st. These are extracting fields from the ParserState st
+        st' = st { offset = offset st + L.length h, string = t }
+    in putState st' ==>&
+       assert (L.length h == n') "end of input" ==>&
+       identity h
+
+
+
+
 
